3f6d384e605e80e5b0c8585c9d27fb770753c5e9
Az1yaT <jeenbek0vv.06@gmail.com>
2022-06-29 22:03:54 +0600

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if(max < b) max = b;
+
+Console.Write("Максимальное число:");
+Console.Write(max);
